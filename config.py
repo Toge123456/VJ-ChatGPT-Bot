40db525b9a59b630c1d287528bd18dbf3c0dc5c4
TOGE INUMAKI <149299533+Toge123456@users.noreply.github.com>
2023-12-28 23:07:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "29539483"))
+API_HASH = environ.get("API_HASH", "ab57ccd3de7bbb16058a80b179ac4294")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6935511939:AAGysFC0HP-lOwBTQvkw2pTS-u0MGENP0Ys")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+ADMINS = int(environ.get("ADMINS", "29539483"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://toge3260:QO4ozHWxuoQeHsry@cluster0.4bedvpu.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
-OPENAI_API = environ.get("OPENAI_API", "")
-AI = is_enabled((environ.get("AI","True")), False)
+OPENAI_API = environ.get("OPENAI_API", "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")
+AI = is_enabled((environ.get("AI","True")), True)
